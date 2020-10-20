e60ef136ea05f5299bc8ccd884471339af94b747
hiteshjangid1 <41499540+hiteshjangid1@users.noreply.github.com>
2020-10-20 23:38:04 +0530

    Work for Day 3

--- src/main/java/com/hitesh/finalmainproject/controllers/DataController.java
+++ src/main/java/com/hitesh/finalmainproject/controllers/DataController.java
@@ -22,4 +22,5 @@ public class DataController {
     public List<BankDetails> getAll(){
         return service.getAll();
     }
+    
 }
--- src/main/java/com/hitesh/finalmainproject/controllers/HomeController.java
+++ src/main/java/com/hitesh/finalmainproject/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.hitesh.finalmainproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hitesh.finalmainproject.models.BankDetails;
+import com.hitesh.finalmainproject.service.BranchService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired BranchService branchService;
 
     @PreAuthorize("permitAll")
     @GetMapping
@@ -19,6 +28,21 @@ public class HomeController {
     public String contactus() {
         return "contactus";
     }
+    
+    @GetMapping("/contactus/add")
+    public String addBranch(Model map)
+    {
+    	BankDetails bank = new BankDetails();
+    	map.addAttribute("bank",bank);
+    	return "addBranch";
+    }
+    
+    @PostMapping("/contactus/add")
+	public String submitBranch(@ModelAttribute("bank") BankDetails bank, Model map) {
+		branchService.save(bank);
+		return "redirect:/contactus";
+	}
+    
     
     @GetMapping("/login")
 	public String getTemplate()
